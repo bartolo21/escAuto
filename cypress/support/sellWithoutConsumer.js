22f1f719622eebe68fb6beff47c74a9231a8291f
Bartłomiej Damek <bartlomiej.damek@mug.pl>
2022-12-14 21:52:56 +0100

    add cart test with no consumer context

--- cypress/support/sellWithoutConsumer.js
+++ cypress/support/sellWithoutConsumer.js
@@ -1,4 +1,28 @@
 import products from "../fixtures/products.json";
 
+//fora dodalem aby podawac ile produktow chcemy dodac do koszyka :)
+//wiem, ze to troche duzy blok ale rozbije go na mniejsze w przyszlosci
+Cypress.Commands.add("addEanAndCheckout", (productsQuantity) => {
+    if (productsQuantity == 1) {
+        cy.get('button').eq(1).click();
+        const randomProduct =  products.productsList[Math.floor(Math.random() * products.productsList.length)];
+        cy.get('div[class="input"] > input[type="tel"]').type(randomProduct);
+        cy.wait(2000)
+        cy.get('button[class="green"]').click();
+        cy.login("card");
+    } else {
+        for(let i = 1; i <= productsQuantity; i++) {
+            cy.get('button').eq(1).click();
+            const randomProduct =  products.productsList[Math.floor(Math.random() * products.productsList.length)];
+            cy.get('div[class="input"] > input[type="tel"]').type(randomProduct);
+            cy.wait(2000)
+            if (i < productsQuantity) {
+                cy.get('div[class="actions-container"]').find("button").first().click();
+            }
+        }
+        cy.get('button[class="green"]').click();
+        cy.login("card");
+    }
+})
 
-const randomProduct =  products.productsList[Math.floor(Math.random() * products.productsList.length)];
+
